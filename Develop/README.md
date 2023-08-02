35aadbde985438a654f292b4e2b305c0a9b7bc39
Elizabeth Quinn <brousy2222@gmail.com>
2023-08-02 09:07:59 -0400

    index.js worked on

---
+++ Develop/README.md
@@ -0,0 +1,3 @@
+# wevwev
+# wevwe
+
---
+++ Develop/index.js
@@ -0,0 +1,64 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown= require('./utils/generateMarkdown');
+const fs = require('fs');
+const path =require('path');
+// TODO: Create an array of questions for user input
+const questions = [
+    // TODO: Move git hub and password to a new section under questions
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your Git Hub username?',
+    },
+    {
+        type: 'input',
+        name: 'password',
+        message: 'What is your Git Hub email address?',
+
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your Project title?',
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your description of the Project?',
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What command should be run to install the dependencies?',
+        default:'npm i',
+
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select what kind of license you want for your Project?',
+        choices:[ "MIT", "Apache 2.0", "GPL", "BSD", "none"]
+    },
+    {}
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { 
+     return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(
+        (data) => {
+            writeToFile('README.md', generateMarkdown({...data}) )
+        }
+    )
+}
+
+// Function call to initialize app
+init();
